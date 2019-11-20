3d55a1edabe7cf5374f309343a9d624cfdc7f4f1
Max-Joseph Krempl <krempl@in.tum.de>
2019-11-21 00:13:05 +0100

    Added check for _getStreamURL.sh helper script

--- _download.sh
+++ _download.sh
@@ -5,11 +5,18 @@ DIR=$(dirname "$0")
 getStreamURL="$DIR/_getStreamURL.sh"
 OUTPUT_DIR="$DIR/saved"
 
+error() {
+	echo -e "$1"
+	exit 1
+}
+
+[ ! -f "$getStreamURL" ] && error "Couldn't find \"$getStreamURL\""
+
+
 if [ $# -ge 2 ]
 then
-	URL=$($DIR/_getStreamURL.sh "$1")
+	URL=$($getStreamURL "$1")
 	ffmpeg -i "$URL" -c copy "${OUTPUT_DIR}/$2.mp4"
 else
-	echo "Usage:  $(basename "$0") {live.rbg.tum.de url} {file name}"
-	exit 1
+	error "Usage:  $(basename "$0") {live.rbg.tum.de url} {file name}"
 fi
