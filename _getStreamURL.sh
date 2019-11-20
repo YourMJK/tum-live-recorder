96e466466513569edf411838fe04351d4b9a735c
Max-Joseph Krempl <krempl@in.tum.de>
2019-11-21 00:01:17 +0100

    Simplified sed call

--- _getStreamURL.sh
+++ _getStreamURL.sh
@@ -5,7 +5,7 @@ EXT="m3u8"
 
 if [ $# -ge 1 ]
 then
-	curl -s "$1" | grep -m1 ".$EXT" | sed "s|.*https|https|g" | sed "s|\.${EXT}.*|\.${EXT}|g"
+	curl -s "$1" | grep -m1 ".$EXT" | sed "s|.*\(http.*\.${EXT}\).*|\1|g"
 else
 	echo "Usage:  $(basename "$0") {live.rbg.tum.de url}"
 	exit 1
